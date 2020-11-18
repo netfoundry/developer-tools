bbacfbc276994564ad62a85e197a28b67e0e8d5f
Kenneth Bingham <w@qrk.us>
2020-11-18 01:40:56 -0500

    add a testing compose file

--- Docker/entrypoint-test.sh
+++ Docker/entrypoint-test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-pip uninstall -y netfoundry
-pip install --editable /python-netfoundry
+#pip uninstall -y netfoundry && pip install --editable /python-netfoundry
+pip install --index-url https://test.pypi.org/simple/ --no-deps --upgrade netfoundry
 
 exec "$@"
