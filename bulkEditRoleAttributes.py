c0035a816840129b3aff4c8b299eebd757200c96
Kenneth Bingham <w@qrk.us>
2020-12-11 16:05:22 -0500

    start using the Python module in the script for bulk editing attributes

--- bulkEditRoleAttributes.py
+++ bulkEditRoleAttributes.py
@@ -1,387 +1,103 @@
 
-import json # operate on structured data
-import requests # HTTP user agent
-import sys # open stderr
+import sys
 import argparse
 import re
 import os
+import netfoundry
 
 #
 # bulkEditRoleAttributes.py
 # blame(ken)
 #
-# bulk edit endpoints' role attributes
+# bulk edit role attributes for Endpoints, Services, or Edge Routers
 #
-
-# describe resource types 
-#  * embedded list key names and
-#  * expected HTTP response codes
-RESOURCETYPES = {
-    'endpoints': {
-        'embedded': "endpointList",
-        'httpcode': "OK"
-    },
-    'edge-routers': {
-        'embedded': "edgeRouterList",
-        'httpcode': "ACCEPTED"
-    },
-    'services': {
-        'embedded': "serviceList",
-        'httpcode': "ACCEPTED"
-    }
-}
-
-MOPENV = 'production'
 
 # print to stderr
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
-# fetch a list of networks matching a name search
-def getNetworkByName(token,name):
-    """return the network object
-        :token required API access token
-        :networkId required name of the NF network may contain quoted whitespace
-    """
-#    name_encoding = urllib.parse.quote(name)
-    try:
-        headers = { 
-            "authorization": "Bearer " + token 
-        }
-        params = {
-            "findByName": name
-        }
-        response = requests.get(
-            'https://gateway.'+MOPENV+'.netfoundry.io/core/v2/networks',
-            headers=headers,
-            params=params
-        )
-
-        http_code = response.status_code
-    except:
-        raise
-
-    if http_code == requests.status_codes.codes.OK: # HTTP 200
-        try:
-            networks = json.loads(response.text)
-        except ValueError as e:
-            eprint('ERROR: failed to load endpoints object from GET response')
-            raise(e)
-    else:
-        raise Exception(
-            'unexpected response: {} (HTTP {:d})'.format(
-                requests.status_codes._codes[http_code][0].upper(),
-                http_code
-            )
-        )
-    hits = networks['page']['totalElements']
-    if hits == 1:
-        network = networks['_embedded']['networkList'][0]
-        return(network)
-    else:
-        raise Exception("ERROR: failed to find exactly one match for {}".format(name))
-
-def getNetwork(token,networkId):
-    """return the network object
-        :token [required] the API access token
-        :networkId [required] the UUID of the NF network
-    """
-    try:
-        headers = { 
-            "authorization": "Bearer " + token 
-        }
-        response = requests.get(
-            'https://gateway.'+MOPENV+'.netfoundry.io/core/v2/networks/'+networkId,
-            headers=headers
-        )
-
-        http_code = response.status_code
-    except:
-        raise
-
-    if http_code == requests.status_codes.codes.OK: # HTTP 200
-        try:
-            network = json.loads(response.text)
-        except ValueError as e:
-            eprint('ERROR: failed to load {r} object from GET response'.format(r = "network"))
-            raise(e)
-    else:
-        raise Exception(
-            'unexpected response: {} (HTTP {:d})'.format(
-                requests.status_codes._codes[http_code][0].upper(),
-                http_code
-            )
-        )
-
-    return(network)
-
-def getResources(token,type,networkId):
-    """return the resources object
-        :token [required] the API access token
-        :type [required] one of endpoints, edge-routers, services
-        :networkId [required] the UUID of the NF network
-    """
-    try:
-        headers = { 
-            "authorization": "Bearer " + token 
-        }
-        params = {
-            "networkId": networkId,
-            "page": 0,
-            "size": 10,
-            "sort": "name,asc"
-        }
-        response = requests.get(
-            'https://gateway.'+MOPENV+'.netfoundry.io/core/v2/'+type,
-            headers=headers,
-            params=params
-        )
-
-        http_code = response.status_code
-    except:
-        raise
-
-    if http_code == requests.status_codes.codes.OK: # HTTP 200
-        try:
-            resources = json.loads(response.text)
-        except ValueError as e:
-            eprint('ERROR: failed to load {r} object from GET response'.format(r = type))
-            raise(e)
-    else:
-        raise Exception(
-            'unexpected response: {} (HTTP {:d})'.format(
-                requests.status_codes._codes[http_code][0].upper(),
-                http_code
-            )
-        )
-
-    total_pages = resources['page']['totalPages']
-    total_elements = resources['page']['totalElements']
-    # if there are no resources
-    if total_elements == 0:
-        return([])
-    # if there is one page of resources
-    elif total_pages == 1:
-        return(resources['_embedded'][RESOURCETYPES[type]['embedded']])
-    # if there are multiple pages of resources
-    else:
-        # initialize the list with the first page of resources
-        all_pages = resources['_embedded'][RESOURCETYPES[type]['embedded']]
-        # append the remaining pages of resources
-        for page in range(1,total_pages):
-            try:
-                params["page"] = page
-                response = requests.get(
-                    'https://gateway.'+MOPENV+'.netfoundry.io/core/v2/'+type,
-                    headers=headers,
-                    params=params
-                )
-                http_code = response.status_code
-            except:
-                raise
-
-            if http_code == requests.status_codes.codes.OK: # HTTP 200
-                try:
-                    resources = json.loads(response.text)
-                    all_pages += resources['_embedded'][RESOURCETYPES[type]['embedded']]
-                except ValueError as e:
-                    eprint('ERROR: failed to load resources object from GET response')
-                    raise(e)
-            else:
-                raise Exception(
-                    'unexpected response: {} (HTTP {:d})'.format(
-                        requests.status_codes._codes[http_code][0].upper(),
-                        http_code
-                    )
-                )
-        return(all_pages)
-
-def patchAttributes(token,type,resource):
-    """return the new resource object
-        :token [required] the API access token
-        :type [required] one of endpoints, edge-routers, services
-        :resource [required] the resource object to PUT 
-    """
-    try:
-        headers = { 
-            'Content-Type': 'application/json',
-            "authorization": "Bearer " + token 
-        }
-        response = requests.patch(
-            'https://gateway.'+MOPENV+'.netfoundry.io/core/v2/'+type+'/'+resource['id'],
-            json={
-                'name': resource['name'],
-                'attributes': resource['attributes']
-            },
-            headers=headers
-        )
-        http_code = response.status_code
-    except:
-        raise
-
-    if http_code == requests.status_codes.codes[RESOURCETYPES[type]['httpcode']]: # HTTP 200
-        try:
-            updated = json.loads(response.text)
-        except ValueError as e:
-            eprint('ERROR: failed to load updated resource object from PUT response body')
-            raise(e)
-    else:
-        raise Exception(
-            'unexpected response: {} (HTTP {:d}\n{})'.format(
-                requests.status_codes._codes[http_code][0].upper(),
-                http_code,
-                response.text
-            )
-        )
-
-    return(updated)
-
-PARSER = argparse.ArgumentParser()
-PARSER.add_argument(
-    "resourceType",
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "resource_type",
     choices={"endpoints", "edge-routers", "services"},
     help="type of resource to update"
 )
-PARSER.add_argument(
+parser.add_argument(
     "-a", "--attributes",
     nargs="+",
     required=True,
-    help="space-sep list of hashtag role attributes e.g. '-a sandbox staging'"
+    help="space-sep list of hashtag role attributes e.g. '-a \#sandbox \#staging'"
 )
-PARSER.add_argument(
-    "-c", "--client-id",
-    default=os.environ.get('NETFOUNDRY_CLIENT_ID',None),
-    dest="clientId",
-    help="required API account principal"
-)
-PARSER.add_argument(
-    "-s", "--client-secret",
-    default=os.environ.get('NETFOUNDRY_CLIENT_SECRET',None),
-    dest="clientSecret",
-    help="required API account credential"
-)
-PARSER.add_argument(
-    "-u", "--networkId",
+parser.add_argument(
+    "-u", "--network-id",
     default=None,
-    dest="networkId",
     help="the UUID of the NF network"
 )
-PARSER.add_argument(
+parser.add_argument(
     "-n", "--network-name",
     default=None,
-    dest="networkName",
+    dest="network_name",
     help="the name of the NF network may contain quoted whitespace"
 )
-PARSER.add_argument(
+parser.add_argument(
     "-i", "--include",
     default=None,
     help="only invite names that match regex"
 )
-PARSER.add_argument(
+parser.add_argument(
     "-e", "--exclude",
     default=None,
     help="do not invite names that match regex"
 )
-PARSER.add_argument(
+parser.add_argument(
     "-v", "--verbose",
     action='store_true',
     default=False,
     help="print INFO level messages"
 )
-PARSER.add_argument(
-    "-t", "--test-environment",
+parser.add_argument(
+    "-c", "--credentials",
     default=None,
-    dest="testEnvironment",
-    help=argparse.SUPPRESS
+    help="API account credentials JSON file"
 )
-ARGS = PARSER.parse_args()
+args = parser.parse_args()
 
-if ARGS.testEnvironment:
-    MOPENV = ARGS.testEnvironment
+session = netfoundry.Session(
+    credentials=args.credentials if args.credentials is not None else None
+)
 
-if os.environ.get('NETFOUNDRY_API_TOKEN',None):
-    TOKEN = os.environ.get('NETFOUNDRY_API_TOKEN')
-else:
-    TOKENREQUEST = {
-        "client_id": ARGS.nfClientId,
-        "client_secret": ARGS.nfClientSecret,
-        "audience": "https://gateway."+MOPENV+".netfoundry.io/",
-        "grant_type": "client_credentials"
-    }
-
-    try:
-        TOKENRESPONSE = requests.post(
-            "https://netfoundry-"+MOPENV+".auth0.com/oauth/token",
-            json=TOKENREQUEST,
-            headers={ 'content-type': "application/json" }
-        )
-        RESPONSECODE = TOKENRESPONSE.status_code
-    except:
-        eprint('ERROR: authentication failed')
-        raise
-
-    if RESPONSECODE == requests.status_codes.codes.OK:
-        try:
-            TOKENOBJECT = json.loads(TOKENRESPONSE.text)
-            TOKEN = TOKENOBJECT['access_token']
-        except:
-            raise Exception(
-                'ERROR: failed to find an access_token in the response and instead got: {}'.format(
-                    TOKENRESPONSE.text
-                )
-            )
-    else:
-        raise Exception(
-            'got unexpected HTTP response {} ({:d})'.format(
-                requests.status_codes._codes[RESPONSECODE][0].upper(),
-                RESPONSECODE
-            )
-        )
-
-
-if ARGS.networkName and ARGS.networkId:
+if args.network_name and args.network_id:
     raise Exception("ERROR: need one of network-name or network-id")
-elif ARGS.networkName:
-    NFNETWORK = getNetworkByName(TOKEN,ARGS.networkName)
-    NFNETWORKID = NFNETWORK['id']
-elif ARGS.networkId:
-    NFNETWORKID = ARGS.networkId
+elif args.network_name:
+    network = netfoundry.Network(session, network_name=args.network_name)
+elif args.network_id:
+    network = netfoundry.Network(session, network_id=args.network_id)
 else:
     raise Exception("ERROR: need one of network-name or network-id")
 
-NETWORKOBJ = getNetwork(TOKEN,NFNETWORKID)
-if ARGS.verbose:
-    print('INFO: operating on network "{n}"'.format(n = NETWORKOBJ['name']))
-RESOURCES = getResources(TOKEN, ARGS.resourceType, NFNETWORKID)
+if args.verbose:
+    print('INFO: operating on network "{:s}"'.format(network.name))
+found_resources = network.get_resources(type=args.resource_type)
 
-ATTRIBUTES = list()
-for attr in ARGS.attributes:
+attributes = list()
+# ensure all attributes begin with a # character
+for attr in args.attributes:
     if not re.match('^[#]', attr):
         attr = '#'+attr
-    ATTRIBUTES.append(attr)
-"""
- for attr in ARGS.attributes:
-    if re.match('^[#]', attr):
-        attr = attr.lstrip('#')
-    ATTRIBUTES.append(attr)
-"""
-for resource in RESOURCES:
+    attributes.append(attr)
 
+for resource in found_resources:
     # skip unless included
-    if ARGS.include and not re.match(ARGS.include, resource['name']):
-        if ARGS.verbose:
+    if args.include and not re.match(args.include, resource['name']):
+        if args.verbose:
             print('INFO: excluded {r}'.format(r = resource['name']))
         continue
-    if ARGS.exclude and re.match(ARGS.exclude, resource['name']):
-        if ARGS.verbose:
+    if args.exclude and re.match(args.exclude, resource['name']):
+        if args.verbose:
             print('INFO: excluded {r}'.format(r = resource['name']))
         continue    
 
-    updated = dict()
-    updated['networkId'] = resource['networkId']
-    updated['id'] = resource['id']
-    updated['name'] = resource['name']
-    updated['attributes'] = ATTRIBUTES
-    patchAttributes(TOKEN, ARGS.resourceType, updated)
-    if ARGS.verbose:
-        print('INFO: updated {r} with {a}'.format(r = resource['name'], a = ATTRIBUTES))
+    resource['attributes'] = attributes
+    network.patch_resource(resource)
+    if args.verbose:
+        print('INFO: updated {r} with {a}'.format(r = resource['name'], a = attributes))
--- bulkInviteEndpoints.py
+++ bulkInviteEndpoints.py
@@ -1,10 +1,9 @@
-import json
 from os import environ
 import sys
 import argparse
 import re
 import jwt
-from datetime import datetime, date
+from datetime import datetime
 import time
 import os
 import netfoundry
@@ -18,69 +17,62 @@ import netfoundry
 #  skipping those that are already registered (.jwt is null) or expired
 #
 
-NOW = time.time()
+now = time.time()
 
 # print to stderr
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
-PARSER = argparse.ArgumentParser()
-PARSER.add_argument(
+parser = argparse.ArgumentParser()
+parser.add_argument(
     "-u", "--network-id",
     default=None,
     help="the UUID of the NF network"
 )
-PARSER.add_argument(
+parser.add_argument(
     "-n", "--network-name",
     default=None,
     help="the name of the NF network may contain quoted whitespace"
 )
-PARSER.add_argument(
+parser.add_argument(
     "-m", "--metadata",
     default=['laptop'],
     nargs="+",
     required=False,
     help="one or more metadata to be appended endpoint names e.g. '-m laptop mobile'"
 )
-PARSER.add_argument(
+parser.add_argument(
     "-a", "--attributes",
     default=[],
     nargs="+",
     required=False,
     help="one or more Endpoint role attributes e.g. -a sandbox staging dummyServices"
 )
-PARSER.add_argument(
+parser.add_argument(
     "-i", "--include",
     default=None,
     help="invite emails matching regex"
 )
-PARSER.add_argument(
+parser.add_argument(
     "-e", "--exclude",
     default=None,
     help="ignore emails matching regex"
 )
-PARSER.add_argument(
+parser.add_argument(
     "-f", "--invitees",
     default=None,
     help="filename with one email address per line like {first}.{last}+{meta}@netfoundry.io where optional {meta} is added to the args of --metadata to compose endpoint names like {first}_{last}-{meta}"
 )
-PARSER.add_argument(
+parser.add_argument(
     "-c", "--credentials",
     default=None,
     help="API account credentials JSON file"
 )
-args = PARSER.parse_args()
+args = parser.parse_args()
 
 session = netfoundry.Session(
-    token=os.environ['NETFOUNDRY_API_TOKEN'] if 'NETFOUNDRY_API_TOKEN' in os.environ else None,
     credentials=args.credentials if args.credentials is not None else None
 )
-
-# yields a list of Network Groups in Organization.networkGroups[], but there's typically only one group
-organization = netfoundry.Organization(session)
-
-# use the default Network Group (the first Network Group ID known to the Organization)
-network_group = netfoundry.NetworkGroup(organization)
 
 if args.network_name and args.network_id:
     raise Exception("ERROR: need one of network-name or network-id")
@@ -97,6 +89,13 @@ if args.invitees:
     invitees = open(args.invitees)
 else:
     invitees = sys.stdin
+
+attributes = list()
+# ensure all attributes begin with a # character
+for attr in args.attributes:
+    if not re.match('^[#]', attr):
+        attr = '#'+attr
+    attributes.append(attr)
 
 for invitee in invitees:
 
@@ -135,19 +134,19 @@ for invitee in invitees:
                 continue
         else:
             # create the endpoint
-            endpoint = network.create_endpoint(name=endpoint_name,attributes=args.attributes)
+            endpoint = network.create_endpoint(name=endpoint_name,attributes=attributes)
 
         # decode the JWT
         expiry_epoch = jwt.decode(endpoint['jwt'], verify=False)['exp']
         # parse the expiry time
         expiry_timestamp = datetime.fromtimestamp(expiry_epoch)
         # recreate if expiring in less than 12 hours
-        if (NOW+(60*60*12)) > expiry_epoch:
+        if (now+(60*60*12)) > expiry_epoch:
             print('EXPIRY: recreating {}'.format(endpoint_name))
             try:
                 network.delete_resource(type="endpoint",id=endpoint['id'])
                 # recreate the endpoint
-                endpoint = network.create_endpoint(name=endpoint_name,attributes=args.attributes)
+                endpoint = network.create_endpoint(name=endpoint_name,attributes=attributes)
             except Exception as e:
                 eprint('ERROR: failed to re-create expired endpoint {}'.format(endpoint_name))
                 raise(e)
