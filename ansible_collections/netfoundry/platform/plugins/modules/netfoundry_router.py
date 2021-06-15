0c8653f015fbb3f1decf99a0e1d17bca5d85fc4e
Kenneth Bingham <w@qrk.us>
2021-06-15 18:41:04 -0400

    add arg rotateKey to Ansible router module to optionally rotate and return the customer router registration key

--- ansible_collections/netfoundry/platform/plugins/modules/netfoundry_router.py
+++ ansible_collections/netfoundry/platform/plugins/modules/netfoundry_router.py
@@ -52,6 +52,11 @@ options:
         type: bool
         required: false
         default: false
+    rotateKey:
+        description: rotate and return the new key for registration of a NetFoundry Linux VM; requires state=PROVISIONED and null dataCenter i.e. NA to NF-hosted routers
+        type: bool
+        required: false
+        default: false
     state:
         description: The desired state
         required: false
@@ -178,6 +183,7 @@ def run_module():
         state=dict(type='str', required=False, default="PROVISIONED", choices=["PROVISIONING", "PROVISIONED", "REGISTERED", "DELETED"]),
         network=dict(type='dict', required=True),
         linkListener=dict(type='bool', required=False, default=False),
+        rotateKey=dict(type='bool', required=False, default=False),
         tunnelerEnabled=dict(type='bool', required=False, default=False),
         wait=dict(type='int', required=False, default=600),
     )
@@ -245,6 +251,9 @@ def run_module():
     if module.params['datacenter']:
         datacenter = module.params['datacenter']
         provider = module.params['provider']
+        # all hosted routers have a link listener
+        if not properties['link_listener']:
+            properties['link_listener'] = True
         # check if UUIDv4
         try: UUID(datacenter, version=4)
         except ValueError:
@@ -274,7 +283,8 @@ def run_module():
     found = network.get_resources(type="edge-routers",name=properties['name'])
     if len(found) == 0:
         if module.params['state'] in ["PROVISIONING", "PROVISIONED", "REGISTERED"]:
-            try: result['message'] = network.create_edge_router(**properties)
+            try:
+                result['message'] = network.create_edge_router(**properties)
             except Exception as e:
                 raise AnsibleError('Failed to create edge router "{}". Caught exception: {}'.format(module.params['name'], to_native(e)))
             result['changed'] = True
@@ -329,7 +339,15 @@ def run_module():
                 except Exception as e:
                     raise AnsibleError('Timed out waiting for status "{}"'.format(module.params['state']))
         elif module.params['state'] == "PROVISIONED":
-                try: network.wait_for_status(module.params['state'], id=router_id, type="edge-router", wait=module.params['wait'], progress=False)
+                try: 
+                    network.wait_for_status(module.params['state'], id=router_id, type="edge-router", wait=module.params['wait'], progress=False)
+                    # if rotate key and is a hosted router then wait for PROVISIONED
+                    if module.params['rotateKey'] and not module.params['datacenter']:
+                        try:
+                            registrationInfo = network.rotate_edge_router_registration(id=router_id)
+                            result['message']['registrationKey'] = registrationInfo['registrationKey']
+                        except Exception as e:
+                            raise AnsibleError('ERROR: failed to rotate and return registration key for router ID "{}"'.format(router_id))
                 except Exception as e:
                     raise AnsibleError('Timed out waiting for status "{}"'.format(module.params['state']))
         # REGISTERED implies PROVISIONED and additionally requires the value of JWT to be null, indicating enrollment
